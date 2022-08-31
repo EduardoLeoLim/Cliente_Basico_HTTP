de4129fcd3b57b37fecf07b4edcb645c84b5d58f
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-08-31 17:33:05 -0500

    Proyecto creado

---
+++ Cliente Basico Http/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Cliente_Basico_Http.Infrastructure.Views;
+
+namespace Cliente_Basico_Http
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            new ClienteHttpView().Show();
+        }
+    }
+}
---
+++ Cliente Basico Http/src/Infrastructure/Views/ClienteHttpView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Cliente_Basico_Http.Infrastructure.Views
+{
+    /// <summary>
+    /// Interaction logic for ClienteHttpView.xaml
+    /// </summary>
+    public partial class ClienteHttpView : Window
+    {
+        public ClienteHttpView()
+        {
+            InitializeComponent();
+        }
+    }
+}
