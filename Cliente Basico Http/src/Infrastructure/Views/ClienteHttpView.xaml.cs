77976ae237a7adfbd9ea61904ce46c5fcca736ce
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-09-01 15:02:25 -0500

    Enviar get request sin parametros

--- Cliente Basico Http/src/Infrastructure/Views/ClienteHttpView.xaml.cs
+++ Cliente Basico Http/src/Infrastructure/Views/ClienteHttpView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Cliente_Basico_Http.Application;
+using Cliente_Basico_Http.Infrastructure.Services;
 
 namespace Cliente_Basico_Http.Infrastructure.Views
 {
@@ -10,6 +14,50 @@ namespace Cliente_Basico_Http.Infrastructure.Views
         public ClienteHttpView()
         {
             InitializeComponent();
+            cbxMetodoHttp.Items.Add("GET");
+            cbxMetodoHttp.Items.Add("POST");
+            cbxMetodoHttp.Items.Add("PUT");
+            cbxMetodoHttp.Items.Add("PATCH");
+            cbxMetodoHttp.Items.Add("DELETE");
+            cbxMetodoHttp.SelectedIndex = 0;
+        }
+
+        private void BtnSendRequest_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (cbxMetodoHttp.SelectedIndex >= 0 && txtUrl.Text.Length > 0)
+            {
+                SendRequest();
+            }
+        }
+
+        private async Task SendRequest()
+        {
+            var sender = new SendRequest(new HttpRequestService());
+            ResponseData responseData = null;
+            switch ((string)cbxMetodoHttp.SelectedValue)
+            {
+                case "GET":
+                    responseData = await sender.SendGetRequest(txtUrl.Text, txtParametros.Text);
+                    break;
+            }
+            
+            if (responseData == null)
+                return;
+            
+            gridRequestResult.Children.Clear();
+            if (rdbRaw.IsChecked == true)
+            {
+                var textResult = new TextBlock();
+                textResult.Text = responseData.Body;
+                gridRequestResult.Children.Add(textResult);
+            }
+
+            if (rdbHtml.IsChecked == true)
+            {
+                var htmlResult = new WebBrowser();
+                htmlResult.NavigateToString(responseData.Body);
+                gridRequestResult.Children.Add(htmlResult);
+            }
         }
     }
 }
