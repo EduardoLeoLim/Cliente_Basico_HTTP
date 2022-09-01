0c07aa588d330046ff9652b8a3d9fcf41592abbd
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-09-01 15:00:46 -0500

    Métodos públicos

--- Cliente Basico Http/src/Application/SendRequest.cs
+++ Cliente Basico Http/src/Application/SendRequest.cs
@@ -16,31 +16,31 @@ public class SendRequest
         _requestService = requestService;
     }
 
-    async Task<ResponseData> SendPostRequest(string url, string parameters)
+    public async Task<ResponseData> SendPostRequest(string url, string parameters)
     {
         List<string> parametersList = parameters.Split("\n").ToList();
         return await new RequestSender(_requestService).SendRequest(Methods.Post, url, parametersList);
     }
 
-    async Task<ResponseData> SendPuttRequest(string url, string parameters)
+    public async Task<ResponseData> SendPutRequest(string url, string parameters)
     {
         List<string> parametersList = parameters.Split("\n").ToList();
         return await new RequestSender(_requestService).SendRequest(Methods.Put, url, parametersList);
     }
 
-    async Task<ResponseData> SendPatchRequest(string url, string parameters)
+    public async Task<ResponseData> SendPatchRequest(string url, string parameters)
     {
         List<string> parametersList = parameters.Split("\n").ToList();
         return await new RequestSender(_requestService).SendRequest(Methods.Patch, url, parametersList);
     }
 
-    async Task<ResponseData> SendGetRequest(string url, string parameters)
+    public async Task<ResponseData> SendGetRequest(string url, string parameters)
     {
         List<string> parametersList = parameters.Split("\n").ToList();
         return await new RequestSender(_requestService).SendRequest(Methods.Get, url, parametersList);
     }
 
-    async Task<ResponseData> SendDeleteRequest(string url, string parameters)
+    public async Task<ResponseData> SendDeleteRequest(string url, string parameters)
     {
         List<string> parametersList = parameters.Split("\n").ToList();
         return await new RequestSender(_requestService).SendRequest(Methods.Delete, url, parametersList);
