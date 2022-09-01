77e4bf6d73ebbccf6e08cea9521da8066993430a
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-08-31 19:10:07 -0500

    Capa de insfrestrucutura

--- Cliente Basico Http/App.xaml.cs
+++ Cliente Basico Http/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Cliente_Basico_Http
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
---
+++ Cliente Basico Http/src/Infrastructure/Services/HttpRequestService.cs
@@ -0,0 +1,11 @@
+﻿using Cliente_Basico_Http.Domain;
+
+namespace Cliente_Basico_Http.Infrastructure.Services;
+
+public class HttpRequestService : RequestService
+{
+    public ResponseHttp SendRequest(RequestHttp requestHttp)
+    {
+        throw new System.NotImplementedException();
+    }
+}
