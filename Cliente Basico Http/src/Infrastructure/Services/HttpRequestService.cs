7c0a3c5f9070078b9e62309d3376d03cf9f64ecd
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-09-01 15:01:16 -0500

    Get Request

--- Cliente Basico Http/src/Infrastructure/Services/HttpRequestService.cs
+++ Cliente Basico Http/src/Infrastructure/Services/HttpRequestService.cs
@@ -1,4 +1,7 @@
-﻿using Cliente_Basico_Http.Domain;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Cliente_Basico_Http.Domain.Enums;
 using Cliente_Basico_Http.Domain.Model;
 using Cliente_Basico_Http.Domain.Services;
 
@@ -6,8 +9,35 @@ namespace Cliente_Basico_Http.Infrastructure.Services;
 
 public class HttpRequestService : RequestService
 {
-    public Response SendRequest(Request request)
+    public async Task<Response> SendRequest(Request request)
     {
-        throw new System.NotImplementedException();
+        using (var client = new HttpClient())
+        {
+            switch (request.Method)
+            {
+                case Methods.Get:
+                    return await SendGetRequest(client, request);
+                case Methods.Post:
+                    break;
+                case Methods.Put:
+                    break;
+                case Methods.Patch:
+                    break;
+                case Methods.Delete:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        return null;
+    }
+
+    private async Task<Response> SendGetRequest(HttpClient client , Request request)
+    {
+        var responseHttpCliente = await client.GetAsync(request.Url);
+        Response response = new Response((int)responseHttpCliente.StatusCode, responseHttpCliente.RequestMessage.ToString(), await responseHttpCliente.Content.ReadAsStringAsync());
+        
+        return response;
     }
 }
