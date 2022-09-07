192ee2eff3fae68ca6cb93aacc8f9bc52d711926
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-09-06 22:38:31 -0500

    Content Type y Status Code agragados

--- Cliente Basico Http/src/Application/ResponseData.cs
+++ Cliente Basico Http/src/Application/ResponseData.cs
@@ -6,15 +6,18 @@ namespace Cliente_Basico_Http.Application;
 public class ResponseData
 {
     public string Body { get; }
+    public string ContentType { get; }
+    public int StatusCode { get; }
 
-    public ResponseData(string body)
+    public ResponseData(int statusCode, string contentType, string body)
     {
         Body = body;
+        ContentType = contentType;
+        StatusCode = statusCode;
     }
-    
+
     public static ResponseData FromAggregate(Response response)
     {
-
-        return new ResponseData(response.Body);
+        return new ResponseData(response.StatusCode, response.ContentType, response.Body);
     }
 }
