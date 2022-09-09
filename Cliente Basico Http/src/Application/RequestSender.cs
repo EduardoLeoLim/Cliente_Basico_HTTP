e6007afacba450cfe7f9fbcc08a58ae8c1eff99f
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-09-08 22:50:49 -0500

    Parámtetros tipo string formato json

--- Cliente Basico Http/src/Application/RequestSender.cs
+++ Cliente Basico Http/src/Application/RequestSender.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Cliente_Basico_Http.Domain.Enums;
 using Cliente_Basico_Http.Domain.Model;
 using Cliente_Basico_Http.Domain.Services;
@@ -9,15 +8,15 @@ namespace Cliente_Basico_Http.Application;
 public class RequestSender
 {
     private readonly RequestService _requestService;
-    
+
     public RequestSender(RequestService requestService)
     {
         _requestService = requestService;
     }
 
-    public async Task<ResponseData> SendRequest(Methods method, string url, Dictionary<string, string> parameters)
+    public async Task<ResponseData> SendRequest(Methods method, string url, string parameters)
     {
         Request request = new Request(method, url, parameters);
-        return ResponseData.FromAggregate( await _requestService.SendRequest(request));
+        return ResponseData.FromAggregate(await _requestService.SendRequest(request));
     }
 }
